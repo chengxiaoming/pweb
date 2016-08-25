b4ad603a6e3fdfa61bfd77337b866f707f4e5689
程晓明 <michael@MacBook-Pro.local>
2016-08-25 17:08:13 +0800

    test

--- learn.py
+++ learn.py
@@ -1,3 +1,27 @@
 print('aaa')
 a = input()
-print a
+print(a)
+
+names = ['bob','michael']
+names.append('cc')
+bc = ('dd','ee')
+print(names,names[0],bc,bc[1])
+av = 0
+if av > 0:
+	print('if in')
+else:
+	print('if out')
+
+
+for i in names:
+	print(i)
+
+dic = {'a':'aa','b':'bb'}
+se = set(['h','h','g','y'])
+print(dic,se)
+
+def power(x,n):
+	y = x*n
+	return y
+
+print(power(2,3))
